e18b896193757b93edbd18d3278eed3c576e7619
Ben Neale <ben0neale@gmail.com>
2023-10-11 18:51:03 +0100

    Character Movement + Control Switching
    Setup Basic movement as well as being able to switch between action maps, therefore changing controls

--- Randomized Controls Platformer/Assets/PlayerController.cs
+++ Randomized Controls Platformer/Assets/PlayerController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private InputMaster inputMaster;
+    private PlayerInput playerInput;
+    private Rigidbody2D RB;
+
+    public float moveSpeed;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        inputMaster = new InputMaster();
+        RB = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        RB.velocity = inputValue.Get<Vector2>() * moveSpeed;
+
+    }
+    private void OnSwitchAction()
+    {
+        if (playerInput.currentActionMap.name == "Player Controls 1")
+        {
+            playerInput.SwitchCurrentActionMap("Player Controls 2");
+        }
+        else
+        {
+            playerInput.SwitchCurrentActionMap("Player Controls 1");
+        }
         
     }
 }
